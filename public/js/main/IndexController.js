80e35944b8a28bbe946bc4ae0e50c4b9f74350e5
Andrii Chernenko <andrii.chern@gmail.com>
2020-05-07 22:43:20 +0300

    Updating worker & page skeleton caching

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -1,6 +1,6 @@
-import PostsView from './views/Posts';
-import ToastsView from './views/Toasts';
-import idb from 'idb';
+import PostsView from "./views/Posts";
+import ToastsView from "./views/Toasts";
+import idb from "idb";
 
 export default function IndexController(container) {
   this._container = container;
@@ -11,12 +11,12 @@ export default function IndexController(container) {
   this._registerServiceWorker();
 }
 
-IndexController.prototype._registerServiceWorker = function() {
+IndexController.prototype._registerServiceWorker = function () {
   if (!navigator.serviceWorker) return;
 
   var indexController = this;
 
-  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+  navigator.serviceWorker.register("/sw.js").then(function (reg) {
     if (!navigator.serviceWorker.controller) {
       return;
     }
@@ -31,82 +31,85 @@ IndexController.prototype._registerServiceWorker = function() {
       return;
     }
 
-    reg.addEventListener('updatefound', function() {
+    reg.addEventListener("updatefound", function () {
       indexController._trackInstalling(reg.installing);
     });
   });
 
-  // TODO: listen for the controlling service worker changing
-  // and reload the page
+  navigator.serviceWorker.addEventListener("controllerchange", function () {
+    location.reload();
+  });
 };
 
-IndexController.prototype._trackInstalling = function(worker) {
+IndexController.prototype._trackInstalling = function (worker) {
   var indexController = this;
-  worker.addEventListener('statechange', function() {
-    if (worker.state == 'installed') {
+  worker.addEventListener("statechange", function () {
+    if (worker.state == "installed") {
       indexController._updateReady(worker);
     }
   });
 };
 
-IndexController.prototype._updateReady = function(worker) {
+IndexController.prototype._updateReady = function (worker) {
   var toast = this._toastsView.show("New version available", {
-    buttons: ['refresh', 'dismiss']
+    buttons: ["refresh", "dismiss"],
   });
 
-  toast.answer.then(function(answer) {
-    if (answer != 'refresh') return;
-    // TODO: tell the service worker to skipWaiting
+  toast.answer.then(function (answer) {
+    if (answer != "refresh") return;
+    worker.postMessage({ skipWaiting: true });
   });
 };
 
 // open a connection to the server for live updates
-IndexController.prototype._openSocket = function() {
+IndexController.prototype._openSocket = function () {
   var indexController = this;
   var latestPostDate = this._postsView.getLatestPostDate();
 
   // create a url pointing to /updates with the ws protocol
-  var socketUrl = new URL('/updates', window.location);
-  socketUrl.protocol = 'ws';
+  var socketUrl = new URL("/updates", window.location);
+  socketUrl.protocol = "ws";
 
   if (latestPostDate) {
-    socketUrl.search = 'since=' + latestPostDate.valueOf();
+    socketUrl.search = "since=" + latestPostDate.valueOf();
   }
 
   // this is a little hack for the settings page's tests,
   // it isn't needed for Wittr
-  socketUrl.search += '&' + location.search.slice(1);
+  socketUrl.search += "&" + location.search.slice(1);
 
   var ws = new WebSocket(socketUrl.href);
 
   // add listeners
-  ws.addEventListener('open', function() {
+  ws.addEventListener("open", function () {
     if (indexController._lostConnectionToast) {
       indexController._lostConnectionToast.hide();
     }
   });
 
-  ws.addEventListener('message', function(event) {
-    requestAnimationFrame(function() {
+  ws.addEventListener("message", function (event) {
+    requestAnimationFrame(function () {
       indexController._onSocketMessage(event.data);
     });
   });
 
-  ws.addEventListener('close', function() {
+  ws.addEventListener("close", function () {
     // tell the user
     if (!indexController._lostConnectionToast) {
-      indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
+      indexController._lostConnectionToast = indexController._toastsView.show(
+        "Unable to connect. Retrying…"
+      );
     }
 
     // try and reconnect in 5 seconds
-    setTimeout(function() {
+    setTimeout(function () {
       indexController._openSocket();
     }, 5000);
   });
 };
 
 // called when the web socket sends message data
-IndexController.prototype._onSocketMessage = function(data) {
+IndexController.prototype._onSocketMessage = function (data) {
   var messages = JSON.parse(data);
   this._postsView.addPosts(messages);
 };
